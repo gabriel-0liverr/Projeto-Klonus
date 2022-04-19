d2cd4c276d4b7b8a724f6d957058d3a5a2295509
gabriel-0liverr <gorgameplay04@gmail.com>
2022-04-19 20:26:00 -0300

    Assets Fases 1 2 3 5

--- player.gd
+++ player.gd
@@ -1,48 +1,29 @@
 extends KinematicBody2D
 
-var velocity = Vector2.ZERO
-var move_speed = 200
-var gravity = 1200
-var jump_force = -720
-var is_grounded
-onready var raycasts = $raycasts
-onready var _animated_sprite = $AnimatedSprite
+var gravity = 40
+var speed = 250
+var motion = Vector2()
 
 func _physics_process(delta):
+	_get_input()
+	_animation()
 	
-	velocity.y += gravity * delta;
-	is_grounded = _check_is_grounded();
-	
-	_get_input();
-	
-	move_and_slide(velocity);
-	
+	move_and_slide(motion)
+
 func _get_input():
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	velocity.x = move_speed * move_direction;
-	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2);
+	var direction_x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	motion.x = direction_x * speed
 	
-	if(move_direction != 0):
-		$AnimatedSprite.scale.x = move_direction;
-
-func _input(event):
-	if Input.is_action_pressed("jump") and is_grounded:
-		velocity.y = jump_force / 2;
-
-
-func _check_is_grounded():
-	for raycast in raycasts.get_children():
-		if(raycast.is_colliding()):
-			return true;
-		return false;
-
-func _process(_delta):
-	if Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left"):
-		if(!is_grounded):
-			_animated_sprite.play("jump")
-		else:
-			_animated_sprite.play("run")
-	elif !is_grounded:
-		_animated_sprite.play("jump")
+	if(direction_x != 0):
+		$AnimatedSprite.scale.x = direction_x
+	
+func _gravity_builder():
+	motion.y += gravity
+	
+func _animation():
+	if(Input.is_action_pressed("ui_right")) or Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.play("run") 
 	else:
-		_animated_sprite.play("idle")
+		$AnimatedSprite.play("idle")
+		
+	
